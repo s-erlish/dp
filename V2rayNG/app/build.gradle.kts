4c2d8b2b63919f0a9a8ded2c03bf557d5e2c55ed
erlish <90829910+s-erlish@users.noreply.github.com>
2026-05-29 06:05:27 +0500

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.departamentvpn.app"
         minSdk = 24
         targetSdk = 37
         versionCode = 731
